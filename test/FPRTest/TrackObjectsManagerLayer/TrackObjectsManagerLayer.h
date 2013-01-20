c9387c144370ea87a24947d01dbebe970333a2e0
Todor Pitekov <esembidi@gmail.com>
2013-01-21 01:54:15 +0200

    created new class TrackObjecsManagerLayer which will be responsible for managing creation, position and removal of track objects

---
+++ test/FPRTest/TrackObjectsManagerLayer/TrackObjectsManagerLayer.h
@@ -0,0 +1,13 @@
+//
+//  TrackObjectsManagerLayer.h
+//  FPRTest
+//
+//  Created by Toshko on 1/21/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+@interface TrackObjectsManagerLayer : CCLayer
+
+@end
---
+++ test/FPRTest/TrackObjectsManagerLayer/TrackObjectsManagerLayer.m
@@ -0,0 +1,21 @@
+//
+//  TrackObjectsManagerLayer.m
+//  FPRTest
+//
+//  Created by Toshko on 1/21/13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "TrackObjectsManagerLayer.h"
+#import "TrackObjectsQueue.h"
+
+
+@interface TrackObjectsManagerLayer ()
+
+@property (nonatomic, retain) TrackObjectsQueue *objectsQueue;
+
+@end
+
+@implementation TrackObjectsManagerLayer
+
+@end
